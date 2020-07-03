f9bdf95373dc0712991a048019ccd514ef8dabce
AndrewKuktenko <andriy.kuktenko@gmail.com>
2020-07-04 02:15:49 +0300

    eslint

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let browser
+let browser;
 
 mp.events.add('loginBrowser', () => {
-    browser = mp.browsers.new('package://public/index.html')
-})
+  browser = mp.browsers.new('package://public/index.html');
+});
 
 // Handle event from server and send data to react app
 mp.events.add('onMessageFromServer', (value) => {
-    browser.execute(`trigger('onMessage', '${value}')`)
-})
+  browser.execute(`trigger('onMessage', '${value}')`);
+});
 
 // Handle event from react app
 mp.events.add('showUrl', (url) => {
-    mp.gui.chat.push(url)
-})
+  mp.gui.chat.push(url);
+});
 
 // F12 - trigger cursor
 mp.keys.bind(0x7B, true, () => {
-    let state = !mp.gui.cursor.visible
-    mp.gui.cursor.show(state, state)
-})
+  const state = !mp.gui.cursor.visible;
+  mp.gui.cursor.show(state, state);
+});
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,22 +2,22 @@ import React, { useEffect } from 'react';
 
 import { Container } from './styles';
 
-const Login = ({}) => {
-    useEffect(() => {
-        
-    }, []);
+const Login = () => {
+  useEffect(() => {
 
-    const click = () => {
+  }, []);
 
-    };
+  const click = () => {
 
-    return (
-        <Container className="app">
-            <h1>Make UI on React!</h1>
-            <p className="current-time">HELLO</p>
-            <p className="message">WORLD</p>
-            <button className="send-button" onClick={() => click()}>Send</button>
-        </Container>
-    );
+  };
+
+  return (
+    <Container className="app">
+      <h1>Make UI on React!</h1>
+      <p className="current-time">HELLO</p>
+      <p className="message">WORLD</p>
+      <button type="button" className="send-button" onClick={() => click()}>Send</button>
+    </Container>
+  );
 };
 export default Login;
