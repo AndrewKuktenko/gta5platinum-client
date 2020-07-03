626da317009e197a2848cdb3672329daaa6bf692
AndrewKuktenko <andriy.kuktenko@gmail.com>
2020-07-04 01:33:42 +0300

    init react app

---
+++ src/components/login/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+import { Container } from './styles';
+
+const Login = ({}) => {
+    useEffect(() => {
+        
+    }, []);
+
+    const click = () => {
+
+    };
+
+    return (
+        <Container className="app">
+            <h1>Make UI on React!</h1>
+            <p className="current-time">HELLO</p>
+            <p className="message">WORLD</p>
+            <button className="send-button" onClick={() => click()}>Send</button>
+        </Container>
+    );
+};
+export default Login;
---
+++ src/components/login/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 80px;
+  box-sizing: border-box;
+  background-color: red;
+  color: white;
+`;
+
+export default Container;
